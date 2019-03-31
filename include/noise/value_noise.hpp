027d9e91cf0c0e8024b0d449a1a5a2a4cedd32e8
Christian Oliveros <christianol_01@hotmail.com>
2019-03-31 14:28:17 -0400

    Formated Document

--- include/noise/value_noise.hpp
+++ include/noise/value_noise.hpp
@@ -9,58 +9,56 @@
 
 #include "utils/int_fit.hpp"
 
-namespace noise {
+namespace noise
+{
 
-template<typename T = float>
-using RemapFunction = T (*) (const T, const T, const T);
+template <typename T = float>
+using RemapFunction = T (*)(const T, const T, const T);
 
-template<typename T>
+template <typename T>
 T cosineRemap(const T, const T, const T);
 
-template<typename T>
+template <typename T>
 T smoothstepRemap(const T, const T, const T);
 
-template<typename T>
+template <typename T>
 T perlinRemap(const T, const T, const T);
 
-template <uint_least16_t Period = 256,typename Engine = std::default_random_engine, typename Result_Type = float, 
-RemapFunction<Result_Type> Remap_Func = smoothstepRemap<Result_Type> >
-class ValueNoise1D 
-{ 
-public:
-
+template <uint_least16_t Period = 256, typename Engine = std::default_random_engine, typename Result_Type = float,
+          RemapFunction<Result_Type> Remap_Func = smoothstepRemap<Result_Type>>
+class ValueNoise1D
+{
+  public:
     static_assert(std::is_floating_point<Result_Type>(), "Result_Type must be a floating point type");
 
     using Dist = typename std::uniform_real_distribution<Result_Type>;
-    using Seed_Type = typename Dist::result_type; 
+    using Seed_Type = typename Dist::result_type;
 
-    ValueNoise1D( Seed_Type seed = 2011 );
+    ValueNoise1D(Seed_Type seed = 2011);
     ~ValueNoise1D();
- 
+
     // Evaluate the noise function at position x
     Result_Type eval(const Result_Type x) const;
 
     // Copy Constructor and Copy Assigment
-    ValueNoise1D(const ValueNoise1D& other);
-    ValueNoise1D& operator=(const ValueNoise1D& other);
+    ValueNoise1D(const ValueNoise1D &other);
+    ValueNoise1D &operator=(const ValueNoise1D &other);
 
     // Move Constructor and Copy Assigment
-    ValueNoise1D(ValueNoise1D&& other) noexcept;
-    ValueNoise1D& operator=(ValueNoise1D&& other) noexcept;
- 
-private:
+    ValueNoise1D(ValueNoise1D &&other) noexcept;
+    ValueNoise1D &operator=(ValueNoise1D &&other) noexcept;
 
+  private:
     using Conv_Type = typename utils::int_least_fit_t<Seed_Type>;
 
     static_assert(Period > 1 && !(Period & (Period - 1)), "Period must be power of 2 different from 0");
-    static constexpr auto kMaxVertices {Period};
-    static constexpr auto kMaxVerticesMask {Period - 1};  
-    static constexpr Result_Type low {0.0};
-    static constexpr Result_Type high {1.0};
-    std::array<Result_Type, kMaxVertices> r {0.0};
- 
+    static constexpr auto kMaxVertices{Period};
+    static constexpr auto kMaxVerticesMask{Period - 1};
+    static constexpr Result_Type low{0.0};
+    static constexpr Result_Type high{1.0};
+    std::array<Result_Type, kMaxVertices> r{0.0};
 };
 
-} // namespace noise 
+} // namespace noise
 
 #endif // !VALUE_NOISE_H
--- src/noise/value_noise.cpp
+++ src/noise/value_noise.cpp
@@ -6,97 +6,104 @@
 #include "utils/lerp.hpp"
 #include "utils/constants.hpp"
 
-namespace noise {
+namespace noise
+{
 
-
-template<typename T>
-T cosineRemap(const T a, const T b, const T t) {
-    assert(t >= 0 && t <= 1); 
-    T tRemapCosine = (1 - std::cos(t * utils::pi<T>)) * 0.5; 
-    return utils::lerp<T>(a, b, tRemapCosine); 
+template <typename T>
+T cosineRemap(const T a, const T b, const T t)
+{
+    assert(t >= 0 && t <= 1);
+    T tRemapCosine = (1 - std::cos(t * utils::pi<T>)) * 0.5;
+    return utils::lerp<T>(a, b, tRemapCosine);
 }
 
-template<typename T>
-T smoothstepRemap(const T a, const T b, const T t) {
-    T tRemapSmoothstep = t * t * (3 - 2 * t); 
-    return utils::lerp<T>(a, b, tRemapSmoothstep); 
+template <typename T>
+T smoothstepRemap(const T a, const T b, const T t)
+{
+    T tRemapSmoothstep = t * t * (3 - 2 * t);
+    return utils::lerp<T>(a, b, tRemapSmoothstep);
 }
 
-template<typename T>
-T perlinRemap(const T a, const T b, const T t) {
-    T tRemapPerlin = t * t * t * (10 - 15 * t + 6 * t * t); 
-    return utils::lerp<T>(a, b, tRemapPerlin); 
+template <typename T>
+T perlinRemap(const T a, const T b, const T t)
+{
+    T tRemapPerlin = t * t * t * (10 - 15 * t + 6 * t * t);
+    return utils::lerp<T>(a, b, tRemapPerlin);
 }
 
-template <uint_least16_t Period, typename Engine, typename Result_Type, 
-    RemapFunction<Result_Type> Remap_Func>
-ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::ValueNoise1D(Seed_Type seed) {
-    Dist distribution {ValueNoise1D::low, ValueNoise1D::high};
-    Engine generator; 
+template <uint_least16_t Period, typename Engine, typename Result_Type,
+          RemapFunction<Result_Type> Remap_Func>
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::ValueNoise1D(Seed_Type seed)
+{
+    Dist distribution{ValueNoise1D::low, ValueNoise1D::high};
+    Engine generator;
 
     generator.seed(seed);
-    for (unsigned i = 0; i < kMaxVertices; ++i) { 
-        r[i] = distribution(generator); 
-    } 
+    for (unsigned i = 0; i < kMaxVertices; ++i)
+    {
+        r[i] = distribution(generator);
+    }
 }
 
 // Copy and Move auto generated members
 
-template <uint_least16_t Period, typename Engine, typename Result_Type, 
-    RemapFunction<Result_Type> Remap_Func>
+template <uint_least16_t Period, typename Engine, typename Result_Type,
+          RemapFunction<Result_Type> Remap_Func>
 ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::~ValueNoise1D() = default;
 
-template <uint_least16_t Period, typename Engine, typename Result_Type, 
-    RemapFunction<Result_Type> Remap_Func>
-ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::ValueNoise1D(const ValueNoise1D& other) = default;
+template <uint_least16_t Period, typename Engine, typename Result_Type,
+          RemapFunction<Result_Type> Remap_Func>
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::ValueNoise1D(const ValueNoise1D &other) = default;
 
-template <uint_least16_t Period, typename Engine, typename Result_Type, 
-    RemapFunction<Result_Type> Remap_Func>
-ValueNoise1D<Period, Engine, Result_Type, Remap_Func>&
-ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::operator=(const ValueNoise1D& other) = default;
+template <uint_least16_t Period, typename Engine, typename Result_Type,
+          RemapFunction<Result_Type> Remap_Func>
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func> &
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::operator=(const ValueNoise1D &other) = default;
 
-template <uint_least16_t Period, typename Engine, typename Result_Type, 
-    RemapFunction<Result_Type> Remap_Func>
-ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::ValueNoise1D(ValueNoise1D&& other) noexcept = default;
+template <uint_least16_t Period, typename Engine, typename Result_Type,
+          RemapFunction<Result_Type> Remap_Func>
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::ValueNoise1D(ValueNoise1D &&other) noexcept = default;
 
-template <uint_least16_t Period, typename Engine, typename Result_Type, 
-    RemapFunction<Result_Type> Remap_Func>
-ValueNoise1D<Period, Engine, Result_Type, Remap_Func>&
-ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::operator=(ValueNoise1D&& other) noexcept = default;
+template <uint_least16_t Period, typename Engine, typename Result_Type,
+          RemapFunction<Result_Type> Remap_Func>
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func> &
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::operator=(ValueNoise1D &&other) noexcept = default;
 
-template <uint_least16_t Period, typename Engine, typename Result_Type, 
-    RemapFunction<Result_Type> Remap_Func>
-Result_Type 
-    ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::eval(const Result_Type x) const {
+template <uint_least16_t Period, typename Engine, typename Result_Type,
+          RemapFunction<Result_Type> Remap_Func>
+Result_Type
+ValueNoise1D<Period, Engine, Result_Type, Remap_Func>::eval(const Result_Type x) const
+{
     // Floor using Integer trunc function
     Conv_Type xi = static_cast<Conv_Type>(x) - (x < 0 && x != static_cast<Conv_Type>(x));
 
-    Result_Type t = x - static_cast<Result_Type>(xi); 
+    Result_Type t = x - static_cast<Result_Type>(xi);
 
     // Modulo using the fact that kMaxVerticesMask is a power of 2
     Conv_Type xMin = xi & static_cast<Conv_Type>(kMaxVerticesMask);
-    Conv_Type xMax = (xMin + 1) & static_cast<Conv_Type>(kMaxVerticesMask); 
+    Conv_Type xMax = (xMin + 1) & static_cast<Conv_Type>(kMaxVerticesMask);
 
+    assert(xMin <= kMaxVertices - 1);
+    assert(xMax <= kMaxVertices - 1);
 
-    assert(xMin <= kMaxVertices - 1); 
-    assert(xMax <= kMaxVertices - 1); 
-
-    return (*Remap_Func)(r[xMin], r[xMax], t);  
-} 
+    return (*Remap_Func)(r[xMin], r[xMax], t);
+}
 
 } // namespace noise
 
 #include <iostream>
 
 int main()
-{ 
+{
     noise::ValueNoise1D valueNoise1D;
-    static const int numSteps = 256; 
- 
-    for (int i = 0; i < numSteps; ++i) { 
+    static const int numSteps = 256;
+
+    for (int i = 0; i < numSteps; ++i)
+    {
         // x varies from -10 to 10
-        float x = (2 * (i / float(numSteps - 1)) - 1) * 10; 
-        std::cout << "Noise at " << x << ": " << valueNoise1D.eval(x) << std::endl; 
-    } 
-    std::cout << "Noise size " << ": " << sizeof(valueNoise1D) << std::endl; 
+        float x = (2 * (i / float(numSteps - 1)) - 1) * 10;
+        std::cout << "Noise at " << x << ": " << valueNoise1D.eval(x) << std::endl;
+    }
+    std::cout << "Noise size "
+              << ": " << sizeof(valueNoise1D) << std::endl;
 }
