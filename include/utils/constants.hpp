de287c95477e102540ace370be6dcacfd6896470
Christian Oliveros <christianol_01@hotmail.com>
2019-03-30 21:04:30 -0400

    PI and E

---
+++ include/utils/constants.hpp
@@ -0,0 +1,15 @@
+
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace utils
+{
+template<typename T>
+constexpr T pi = T(3.14159265358979323846264338327950288L);
+
+template<typename T>
+constexpr T exp = T(2.71828182845904523536028747135266250L);
+}
+
+#endif // !CONSTANTS_H
