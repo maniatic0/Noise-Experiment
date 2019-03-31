5890b3cf61fed58f9b6fc5f93ebf717c20642b89
Christian Oliveros <christianol_01@hotmail.com>
2019-03-30 21:20:45 -0400

    Static cast constants

--- include/utils/constants.hpp
+++ include/utils/constants.hpp
@@ -6,10 +6,10 @@
 namespace utils
 {
 template<typename T>
-constexpr T pi = T(3.14159265358979323846264338327950288L);
+constexpr T pi = static_cast<T>(3.14159265358979323846264338327950288L);
 
 template<typename T>
-constexpr T exp = T(2.71828182845904523536028747135266250L);
+constexpr T exp = static_cast<T>(2.71828182845904523536028747135266250L);
 }
 
 #endif // !CONSTANTS_H
