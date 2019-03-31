35bdcb3bc23df8730c29a467b95aee2128a864da
Christian Oliveros <christianol_01@hotmail.com>
2019-03-30 22:13:00 -0400

    Added Remap Function Support

--- include/noise/value_noise.hpp
+++ include/noise/value_noise.hpp
@@ -9,29 +9,43 @@
 #include "utils/int_fit.hpp"
 
 namespace noise {
-template <typename Engine = std::default_random_engine, typename T = float>
+
+template<typename T = float>
+using RemapFunction = T (*) (const T, const T, const T);
+
+template<typename T>
+T cosineRemap(const T, const T, const T);
+
+template<typename T>
+T smoothstepRemap(const T, const T, const T);
+
+template<typename T>
+T perlinRemap(const T, const T, const T);
+
+template <typename Engine = std::default_random_engine, typename Result_Type = float, 
+RemapFunction<Result_Type> Remap_Func = smoothstepRemap<Result_Type> >
 class ValueNoise1D 
 { 
 public:
 
-    static_assert(std::is_floating_point<T>(), "T must be a floating point type");
+    static_assert(std::is_floating_point<Result_Type>(), "Result_Type must be a floating point type");
 
-    using Dist = std::uniform_real_distribution<T>;
+    using Dist = typename std::uniform_real_distribution<Result_Type>;
     using Seed_Type = typename Dist::result_type; 
 
     ValueNoise1D( Seed_Type seed = 2011 );
  
     // Evaluate the noise function at position x
-    T eval(const T x);
+    Result_Type eval(const Result_Type x);
  
 private:
 
-    using Conv_Type = typename utils::uint_least_fit<Seed_Type>::type;
+    using Conv_Type = typename utils::int_least_fit<Seed_Type>::type;
 
     static constexpr unsigned kMaxVertices = {10}; 
-    static constexpr T low = {0.0};
-    static constexpr T high = {1.0};
-    std::array<T, kMaxVertices> r {0.0};
+    static constexpr Result_Type low = {0.0};
+    static constexpr Result_Type high = {1.0};
+    std::array<Result_Type, kMaxVertices> r {0.0};
 
     Dist distribution {ValueNoise1D::low, ValueNoise1D::high};
     Engine generator; 
@@ -39,4 +53,4 @@ private:
 
 } // namespace noise 
 
-#endif // VALUE_NOISE_H
+#endif // !VALUE_NOISE_H
--- src/noise/value_noise.cpp
+++ src/noise/value_noise.cpp
@@ -1,28 +1,54 @@
 
 #include <cassert>
+#include <cmath>
 
 #include "noise/value_noise.hpp"
 #include "utils/lerp.hpp"
+#include "utils/constants.hpp"
 
 namespace noise {
 
-template <typename Engine, typename T>
-ValueNoise1D<Engine, T>::ValueNoise1D(Seed_Type seed) {
+
+template<typename T>
+T cosineRemap(const T a, const T b, const T t) {
+    assert(t >= 0 && t <= 1); 
+    T tRemapCosine = (1 - std::cos(t * utils::pi<T>)) * 0.5; 
+    return utils::lerp<T>(a, b, tRemapCosine); 
+}
+
+template<typename T>
+T smoothstepRemap(const T a, const T b, const T t) {
+    T tRemapSmoothstep = t * t * (3 - 2 * t); 
+    return utils::lerp<T>(a, b, tRemapSmoothstep); 
+}
+
+template<typename T>
+T perlinRemap(const T a, const T b, const T t) {
+    T tRemapPerlin = t * t * t * (10 - 15 * t + 6 * t * t); 
+    return utils::lerp<T>(a, b, tRemapPerlin); 
+}
+
+template <typename Engine, typename Result_Type, RemapFunction<Result_Type> Remap_Func>
+ValueNoise1D<Engine, Result_Type, Remap_Func>::ValueNoise1D(Seed_Type seed) {
     generator.seed(seed);
     for (unsigned i = 0; i < kMaxVertices; ++i) { 
         r[i] = distribution(generator); 
     } 
 }
 
-template <typename Engine, typename T>
-T ValueNoise1D<Engine, T>::eval(const T x) {
-    assert(x >= 0); 
-    // Integer floor function
-    Conv_Type xMin = static_cast<Conv_Type>(x); 
-    assert(xMin <= kMaxVertices - 1); 
-    T t = x - static_cast<T>(xMin); 
+template <typename Engine, typename Result_Type, RemapFunction<Result_Type> Remap_Func>
+Result_Type ValueNoise1D<Engine, Result_Type, Remap_Func>::eval(const Result_Type x) {
+    // Integer trunc function
+    Conv_Type xi = static_cast<Conv_Type>(x);; 
+    Conv_Type xMin = xi % static_cast<Conv_Type>(kMaxVertices);
+    Result_Type t = x - static_cast<Result_Type>(xi); 
+    Conv_Type xMax = (xMin == static_cast<Conv_Type>(kMaxVertices) - 1) ? 0 : xMin + 1; 
 
-    return utils::lerp(r[xMin], r[xMin + 1], t); 
+
+    assert(xMin <= kMaxVertices - 1); 
+    assert(xMax <= kMaxVertices - 1); 
+
+    return (*Remap_Func)(r[xMin], r[xMax], t);  
 } 
 
 } // namespace noise
@@ -31,8 +57,9 @@ T ValueNoise1D<Engine, T>::eval(const T x) {
 
 int main()
 { 
+    std::cout << noise::cosineRemap<float>(0,1,1)<< std::endl;
     noise::ValueNoise1D noise;
-    for(size_t i = 0; i < 8; i++)
+    for(size_t i = 85; i < 100; i++)
     {
         std::cout << noise.eval(0.5 + (float) i) << std::endl;
     }
