d1fa32ff132adafd1f732f3c31368ca322f7d398
Christian Oliveros <christianol_01@hotmail.com>
2019-03-31 22:44:11 -0400

    Vec2 Example

---
+++ include/vec/vec2.hpp
@@ -0,0 +1,21 @@
+
+
+#ifndef VEC_2_H
+
+namespace vector
+{
+template <typename T = float>
+class Vec2
+{
+  public:
+    Vec2() : x(T(0)), y(T(0)) {}
+    Vec2(T xx, T yy) : x(xx), y(yy) {}
+    Vec2 operator*(const T &r) const;
+    T x, y;
+};
+
+using Vec2f = Vec2<float>;
+
+} // namespace vector
+
+#endif // !VEC_2_H
