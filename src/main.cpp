f8dee51cb4e2d369f7757df7ff9344afd45835ff
Christian Oliveros <christianol_01@hotmail.com>
2019-04-02 00:32:15 -0400

    Fixed Include

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <fstream> 
+#include <cstdio> 
+#include <random> 
+#include <functional> 
+
+
 #include "noise/value_noise.hpp"
+#include "vec/vec2.hpp"
 
 int main()
 {
@@ -13,6 +20,48 @@ int main()
         std::cout << "Noise at " << x << ": " << valueNoise1D.eval(x) << std::endl;
     }
 
+
+    unsigned imageWidth = 512; 
+    unsigned imageHeight = 512; 
+    float *noiseMap = new float[imageWidth * imageHeight]; 
+#if 0 
+    // generate white noise
+    unsigned seed = 2016; 
+    std::mt19937 gen(seed); 
+    std::uniform_real_distribution distr; 
+    auto dice = std::bind(distr, gen); // std::function<float()> 
+ 
+    for (unsigned j = 0; j < imageHeight; ++j) { 
+        for (unsigned i = 0; i < imageWidth; ++i) { 
+            // generate a float in the range [0:1]
+            noiseMap[j * imageWidth + i] = dice(); 
+        } 
+    } 
+#else 
+    // generate value noise
+    noise::ValueNoise2D noise; 
+    float frequency = 0.05f; 
+    for (unsigned j = 0; j < imageHeight; ++j) { 
+        for (unsigned i = 0; i < imageWidth; ++i) { 
+            // generate a float in the range [0:1]
+            noiseMap[j * imageWidth + i] = noise.eval(vector::Vec2<float>(i, j) * frequency); 
+        } 
+    } 
+#endif 
+ 
+    // output noise map to PPM
+    std::ofstream ofs; 
+    ofs.open("./noise.ppm", std::ios::out | std::ios::binary); 
+    ofs << "P6\n" << imageWidth << " " << imageHeight << "\n255\n"; 
+    for (unsigned k = 0; k < imageWidth * imageHeight; ++k) { 
+        unsigned char n = static_cast<unsigned char>(noiseMap[k] * 255); 
+        ofs << n << n << n; 
+    } 
+    ofs.close(); 
+ 
+    delete[] noiseMap; 
+
+
     noise::ValueNoise2D valueNoise2D;
 
     std::cout << "ValueNoise1D size "
@@ -20,4 +69,6 @@ int main()
 
     std::cout << "ValueNoise1D size "
               << ": " << sizeof(valueNoise2D) << std::endl;
+
+    return 0; 
 }
