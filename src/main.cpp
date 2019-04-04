f3225037fa2fc8c8449b7cdd24040660a64ebbf0
Christian Oliveros <christianol_01@hotmail.com>
2019-04-03 20:01:10 -0400

    Noise Examples

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
-#include <fstream>
 #include <cstdio>
-#include <random>
+#include <fstream>
 #include <functional>
+#include <iostream>
+#include <random>
 
 #include "noise/value_noise.hpp"
 #include "vec/vec2.hpp"
@@ -54,18 +54,18 @@ int main()
   }
 #else
   noise::ValueNoiseND<2, 512> noise;
+
+#if 0
+
   unsigned numLayers = 5;
   float maxNoiseVal = 0;
   float frequency = 0.01f;
-  for (unsigned j = 0; j < imageHeight; ++j)
-  {
-    for (unsigned i = 0; i < imageWidth; ++i)
-    {
+  for (unsigned j = 0; j < imageHeight; ++j) {
+    for (unsigned i = 0; i < imageWidth; ++i) {
       vector::Vec2f pNoise = vector::Vec2f(i, j) * frequency;
       float amplitude = 1.0f / frequency;
       noiseMap[j * imageWidth + i] = 0;
-      for (unsigned l = 0; l < numLayers; ++l)
-      {
+      for (unsigned l = 0; l < numLayers; ++l) {
         noiseMap[j * imageWidth + i] += noise.eval(pNoise) * amplitude;
         pNoise *= 2.0;
         amplitude *= 0.5;
@@ -75,10 +75,68 @@ int main()
     }
   }
 
-  for (size_t i = 0; i < imageWidth * imageHeight; i++)
-  {
+  for (size_t i = 0; i < imageWidth * imageHeight; i++) {
     noiseMap[i] /= maxNoiseVal;
   }
+
+#else
+  float frequency = 0.02f;
+  float frequencyMult = 1.8; // lacunarity
+  float amplitudeMult = 0.35;
+  unsigned numLayers = 5;
+  float maxNoiseVal = 0;
+  for (unsigned j = 0; j < imageHeight; ++j)
+  {
+    for (unsigned i = 0; i < imageWidth; ++i)
+    {
+      vector::Vec2f pNoise = vector::Vec2f(i, j) * frequency;
+      float amplitude = 1;
+      noiseMap[j * imageWidth + i] = 0;
+      for (unsigned l = 0; l < numLayers; ++l)
+      {
+//#define TURBULENCE
+#ifdef TURBULENCE
+#include <cmath>
+        noiseMap[j * imageWidth + i] +=
+            std::fabs(2 * noise.eval(pNoise) - 1) * amplitude;
+#else
+        noiseMap[j * imageWidth + i] += noise.eval(pNoise) * amplitude;
+#endif // !TURBULENCE
+
+        pNoise *= frequencyMult;
+        amplitude *= amplitudeMult;
+      }
+
+//#define MARBEL_TEXTURE
+#define WOOD_TEXTURE
+#ifdef MARBEL_TEXTURE
+#include "utils/constants.hpp"
+#include <cmath>
+
+      noiseMap[j * imageWidth + i] =
+          (std::sin((i + noiseMap[j * imageWidth + i] * 100) * 2 *
+                    utils::pi<float> / 200.f) +
+           1) /
+          2.f;
+      maxNoiseVal = 1.0f;
+#elif defined(WOOD_TEXTURE)
+#include "utils/fast_convertion.hpp"
+      constexpr int grain = 4; // Wood Grain
+      float g = noise.eval(vector::Vec2f(i, j) * frequency) * grain; 
+      noiseMap[j * imageWidth + i] = g - static_cast<int>(g);
+      maxNoiseVal = 1.0f;
+#else
+      if (noiseMap[j * imageWidth + i] > maxNoiseVal)
+      {
+        maxNoiseVal = noiseMap[j * imageWidth + i];
+      }
+#endif // MARBEL_TEXTURE
+
+    }
+  }
+  for (unsigned i = 0; i < imageWidth * imageHeight; ++i)
+    noiseMap[i] /= maxNoiseVal;
+#endif // 0
 
 #endif
 #endif
