b8c04ff412e360910ff251c0c5090d4524f9087c
Christian Oliveros <christianol_01@hotmail.com>
2019-04-23 22:01:26 -0400

    Refactored Main Little tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,25 @@
 #include <functional>
 #include <iostream>
 #include <random>
+#include <string>
 
 #include "noise/perlin_noise.hpp"
 #include "noise/value_noise.hpp"
 #include "vec/vec2.hpp"
 #include "vec/vec3.hpp"
+
+void save2PPM(const char *filename, unsigned imageWidth, unsigned imageHeight,
+              float *noiseMap) {
+  // output noise map to PPM
+  std::ofstream ofs;
+  ofs.open(filename, std::ios::out | std::ios::binary);
+  ofs << "P6\n" << imageWidth << " " << imageHeight << "\n255\n";
+  for (unsigned k = 0; k < imageWidth * imageHeight; ++k) {
+    unsigned char n = static_cast<unsigned char>(noiseMap[k] * 255);
+    ofs << n << n << n;
+  }
+  ofs.close();
+}
 
 int main() {
   noise::PerlinNoise noiseTest;
@@ -26,60 +40,73 @@ int main() {
   unsigned imageWidth = 512;
   unsigned imageHeight = 512;
   float *noiseMap = new float[imageWidth * imageHeight];
-#if 0 
-    // generate white noise
-    unsigned seed = 2016; 
-    std::mt19937 gen(seed); 
-    std::uniform_real_distribution distr; 
-    auto dice = std::bind(distr, gen); // std::function<float()> 
- 
-    for (unsigned j = 0; j < imageHeight; ++j) { 
-        for (unsigned i = 0; i < imageWidth; ++i) { 
-            // generate a float in the range [0:1]
-            noiseMap[j * imageWidth + i] = dice(); 
-        } 
-    }
-#else
-#if 0
-  // generate value noise
-  noise::ValueNoise2D noise;
-  float frequency = 0.05f;
-  for (unsigned j = 0; j < imageHeight; ++j)
-  {
-    for (unsigned i = 0; i < imageWidth; ++i)
-    {
-      // generate a float in the range [0:1]
-      noiseMap[j * imageWidth + i] = noise.eval(vector::Vec2<float>(i, j) * frequency);
-    }
-  }
-#else
-  noise::ValueNoiseND<2, 512> noise;
 
-#if 0
+  // generate white noise
+  unsigned seed = 2016;
+  std::mt19937 gen(seed);
+  std::uniform_real_distribution distr;
+  auto dice = std::bind(distr, gen); // std::function<float()>
 
-  unsigned numLayers = 5;
-  float maxNoiseVal = 0;
-  float frequency = 0.01f;
   for (unsigned j = 0; j < imageHeight; ++j) {
     for (unsigned i = 0; i < imageWidth; ++i) {
-      vector::Vec2f pNoise = vector::Vec2f(i, j) * frequency;
-      float amplitude = 1.0f / frequency;
-      noiseMap[j * imageWidth + i] = 0;
-      for (unsigned l = 0; l < numLayers; ++l) {
-        noiseMap[j * imageWidth + i] += noise.eval(pNoise) * amplitude;
-        pNoise *= 2.0;
-        amplitude *= 0.5;
-      }
-      if (noiseMap[j * imageWidth + i] > maxNoiseVal)
-        maxNoiseVal = noiseMap[j * imageWidth + i];
+      // generate a float in the range [0:1]
+      noiseMap[j * imageWidth + i] = dice();
     }
   }
 
-  for (size_t i = 0; i < imageWidth * imageHeight; i++) {
-    noiseMap[i] /= maxNoiseVal;
+  // output white noise map to PPM
+  save2PPM("./white_noise.ppm", imageWidth, imageHeight, noiseMap);
+  delete[] noiseMap;
+  noiseMap = new float[imageWidth * imageHeight];
+
+  noise::ValueNoise2D noise;
+  {
+    // generate value noise
+    float frequency = 0.05f;
+    for (unsigned j = 0; j < imageHeight; ++j) {
+      for (unsigned i = 0; i < imageWidth; ++i) {
+        // generate a float in the range [0:1]
+        noiseMap[j * imageWidth + i] =
+            noise.eval(vector::Vec2<float>(i, j) * frequency);
+      }
+    }
   }
 
-#else
+  // output value noise map to PPM
+  save2PPM("./value_noise.ppm", imageWidth, imageHeight, noiseMap);
+  delete[] noiseMap;
+  noiseMap = new float[imageWidth * imageHeight];
+
+  // Brown Noise
+  {
+    unsigned numLayers = 5;
+    float maxNoiseVal = 0;
+    float frequency = 0.01f;
+    for (unsigned j = 0; j < imageHeight; ++j) {
+      for (unsigned i = 0; i < imageWidth; ++i) {
+        vector::Vec2f pNoise = vector::Vec2f(i, j) * frequency;
+        float amplitude = 1.0f / frequency;
+        noiseMap[j * imageWidth + i] = 0;
+        for (unsigned l = 0; l < numLayers; ++l) {
+          noiseMap[j * imageWidth + i] += noise.eval(pNoise) * amplitude;
+          pNoise *= 2.0;
+          amplitude *= 0.5;
+        }
+        if (noiseMap[j * imageWidth + i] > maxNoiseVal)
+          maxNoiseVal = noiseMap[j * imageWidth + i];
+      }
+    }
+
+    for (size_t i = 0; i < imageWidth * imageHeight; i++) {
+      noiseMap[i] /= maxNoiseVal;
+    }
+  }
+
+  // output brown noise map to PPM
+  save2PPM("./brown_noise.ppm", imageWidth, imageHeight, noiseMap);
+  delete[] noiseMap;
+  noiseMap = new float[imageWidth * imageHeight];
+
   float frequency = 0.02f;
   float frequencyMult = 1.8; // lacunarity
   float amplitudeMult = 0.35;
@@ -131,21 +158,9 @@ int main() {
   }
   for (unsigned i = 0; i < imageWidth * imageHeight; ++i)
     noiseMap[i] /= maxNoiseVal;
-#endif // 0
-
-#endif
-#endif
 
   // output noise map to PPM
-  std::ofstream ofs;
-  ofs.open("./noise.ppm", std::ios::out | std::ios::binary);
-  ofs << "P6\n" << imageWidth << " " << imageHeight << "\n255\n";
-  for (unsigned k = 0; k < imageWidth * imageHeight; ++k) {
-    unsigned char n = static_cast<unsigned char>(noiseMap[k] * 255);
-    ofs << n << n << n;
-  }
-  ofs.close();
-
+  save2PPM("./noise.ppm", imageWidth, imageHeight, noiseMap);
   delete[] noiseMap;
 
   noise::ValueNoise1D valueNoise1D;
